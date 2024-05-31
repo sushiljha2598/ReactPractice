aa5698983fd82ebe6233a521721b86207f56385c
sushiljha2598 <78631531+sushiljha2598@users.noreply.github.com>
2024-05-31 12:01:21 +0530

    Added FetchAPI code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_ENDPOINT = `https://jsonplaceholder.typicode.com/users`;
+  const fetchUsers = async (uri) => {
+    try {
+      const response = await fetch(uri);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  useEffect(()=>{
+    fetchUsers(API_ENDPOINT)
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>Fetch API</div>
   )
 }
 
