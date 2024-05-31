4e2f52a8c85592fbd4c5bb7f355bba4a1e3dd622
sushiljha2598 <78631531+sushiljha2598@users.noreply.github.com>
2024-05-31 12:20:29 +0530

    Implemented setTimeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [message, setMessage] = useState("Hello World!");
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setMessage("Goodbye World!!!")
+    }, 5000);
+    return () => clearTimeout(timer)
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>
+        Message : {message}
+      </h1>
+    </div>
   )
 }
 
